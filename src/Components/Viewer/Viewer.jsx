71b318e554e92a3279b4f14afd3f1e8677a7498b
Iambluee <81121072+itsBllue@users.noreply.github.com>
2022-03-24 01:31:50 +0000

    Feat: [Viewer] Creating viewer base
    added render functions and dummy data

--- src/Components/Viewer/Viewer.jsx
+++ src/Components/Viewer/Viewer.jsx
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
 import './Viewer.css';
-export  default class Viewer extends Component {
+import Projects from './DummyData.json'
+import {Transition} from 'react-transition-group';
+export  default class Viewer extends React.Component {
 
+    state={}
     constructor(props){
         super(props);
-        this.state = {
+        console.log('Projects', Projects, Object.keys(Projects).length)
+         this.state = {
             CurrentItemInView : undefined,
-            ViewList : undefined,
+            ViewList : Projects,
             // fetchView : d(),
         }
     }
-    componentDidMount(){
-        let localState = state
-        localState = {...this.props}
-        this.setState(localState)
-    }
+    // componentDidMount(){
+    //     console.log(Projects)
+    //     // let localState = this.state
+    //     // localState = {...this.props}
+    //     // this.setState(localState)
+    // }
 
     render(){
         // const props = this.props;
+        
         const localstate  = this.state
-        const numberOfItems = state.CurrentItemInView == undefined ? false: localstate.CurrentItemInView.length
+        const numberOfItems = localstate.ViewList == undefined ? false: Object.keys(localstate.ViewList).length
+        console.log('Projects', numberOfItems, Object.keys(localstate.ViewList).length)
         return(
         <div className="Viewer">
-                {numberOfItems > 0  ? <FullView/> : <Loader/>}
+                {numberOfItems  == undefined  ? <Loader/> :localstate.CurrentItemInView ? <FullView/> : <ProjectViewer list={localstate.ViewList}/>}
         </div>
         )
     }
@@ -40,4 +47,27 @@ function Loader(){
     return(
         <div className="Loader">Loader</div>
     )
+}
+
+
+function ProjectViewer(props){
+    let list = Object.keys(props.list).map((item,value)=>{
+            // console.log(props.list[item])
+            let listItem = props.list[item]
+        return<ListItem itemName={listItem.Display}></ListItem>})
+    // let list = Object.keys(props.list)
+    console.log(list)
+    return(
+        <>{list}</>
+        // <Transition>
+            // {/* {list} */}
+        // </Transition>
+    )
+}
+
+function ListItem(props){
+    return(<div className='Item'>
+    <h1>{props.itemName}</h1>
+    <div className='test'></div>
+    </div>)
 }
