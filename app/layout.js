4785a01b6fb688220d62fa3dad7f4396d7ae4496
Tan <barabig4@gmail.com>
2023-05-09 19:07:01 +0100

    fix: google analytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Sidebar from './components/sidebar'
 import DrawerIcon from './components/icons/DrawerIcon'
+import GoogleAnalytics from './components/googleAnalytics'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -12,7 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className=' text-xs sm:text-sm md:text-base'>
+        
       <body className="flex flex-col w-screen lg:w-full min-h-screen overflow-x-hidden">
+        <GoogleAnalytics/>
             {/* <div className='drawer drawer-mobile'>
 
               <Sidebar/>
